8d7180fc4bc482bf9fcfb7743e2ead72fd7c4c8a
xudengqi <291899430@qq.com>
2016-07-19 16:22:04 +0800

    update hello.java
    输出34

--- src/com/hello/java/Hello.java
+++ src/com/hello/java/Hello.java
@@ -3,6 +3,6 @@ package com.hello.java;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("1111");
-		System.out.println("3333");
+		System.out.println("3344");
 	}
 }
